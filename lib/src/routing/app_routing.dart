15545c2e3d94b9f9d1f91e89fa14ba97cb0ddcfd
Jashwanth-Gowda-R <jashwanth.go@gmail.com>
2022-11-22 19:17:43 +0530

    added new file for routing

---
+++ lib/src/routing/app_routing.dart
@@ -0,0 +1,25 @@
+import 'package:ecommerce_app/src/features/products_list/products_list_screen.dart';
+import 'package:ecommerce_app/src/features/shopping_cart/shopping_cart_screen.dart';
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+
+final goRoutes = GoRouter(
+  debugLogDiagnostics: true,
+  initialLocation: '/',
+  routes: [
+    GoRoute(
+        path: '/',
+        builder: (context, state) => const ProductsListScreen(),
+        routes: [
+          GoRoute(
+            path: 'cart',
+            // builder: (context, state) => const ShoppingCartScreen(),
+            pageBuilder: (context, state) => MaterialPage(
+              key: state.pageKey,
+              child: const ShoppingCartScreen(),
+              fullscreenDialog: true,
+            ),
+          ),
+        ]),
+  ],
+);
