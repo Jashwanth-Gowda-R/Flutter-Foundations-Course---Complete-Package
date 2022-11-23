08cbea7c440e54e795aad9b445830af893d03041
Jashwanth-Gowda-R <jashwanth.go@gmail.com>
2022-11-23 17:43:26 +0530

    error handling using gorouter

--- lib/src/routing/app_routing.dart
+++ lib/src/routing/app_routing.dart
@@ -1,5 +1,7 @@
 import 'package:ecommerce_app/src/features/account/account_screen.dart';
+import 'package:ecommerce_app/src/features/not_found/not_found_screen.dart';
 import 'package:ecommerce_app/src/features/orders_list/orders_list_screen.dart';
+import 'package:ecommerce_app/src/features/product_page/product_screen.dart';
 import 'package:ecommerce_app/src/features/products_list/products_list_screen.dart';
 import 'package:ecommerce_app/src/features/shopping_cart/shopping_cart_screen.dart';
 import 'package:ecommerce_app/src/features/sign_in/email_password_sign_in_screen.dart';
@@ -13,6 +15,7 @@ enum AppRoutes {
   account,
   orders,
   signIn,
+  product,
 }
 
 final goRouter = GoRouter(
@@ -24,6 +27,14 @@ final goRouter = GoRouter(
       name: AppRoutes.home.name,
       builder: (context, state) => const ProductsListScreen(),
       routes: [
+        GoRoute(
+          path: 'product/:id',
+          name: AppRoutes.product.name,
+          builder: (context, state) {
+            final productId = state.params['id']!;
+            return ProductScreen(productId: productId);
+          },
+        ),
         GoRoute(
           path: 'cart',
           name: AppRoutes.cart.name,
@@ -65,4 +76,5 @@ final goRouter = GoRouter(
       ],
     ),
   ],
+  errorBuilder: (context, state) => const NotFoundScreen(),
 );
