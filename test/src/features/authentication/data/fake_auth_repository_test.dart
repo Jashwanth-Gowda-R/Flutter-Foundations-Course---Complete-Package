7257f7f54584e007adc19d6d3a2dd2e8151b7bd8
Jashwanth-Gowda-R <jashwanth.go@gmail.com>
2022-12-11 20:35:22 +0530

    Testing the FakeAuthRepository (part 2) + advanced stream matchers

--- test/src/features/authentication/data/fake_auth_repository_test.dart
+++ test/src/features/authentication/data/fake_auth_repository_test.dart
@@ -37,5 +37,52 @@ void main() {
         );
       }),
     );
+
+    test(
+      "Current user is not null when user is registered",
+      (() async {
+        final authRepository = makeFakeAuthRepository();
+        await authRepository.createUserWithEmailAndPassword(
+            testEmail, testPassword);
+        expect(authRepository.currentUser, testUser);
+        expect(
+          authRepository.authStateChanges(),
+          emits(testUser),
+        );
+      }),
+    );
+
+    test(
+      "Current user is null when user is signed out",
+      (() async {
+        final authRepository = makeFakeAuthRepository();
+        await authRepository.signInWithEmailAndPassword(
+            testEmail, testPassword);
+        expect(authRepository.currentUser, testUser);
+        expect(
+          authRepository.authStateChanges(),
+          emits(testUser),
+        );
+        await authRepository.signOut();
+        expect(authRepository.currentUser, null);
+        expect(
+          authRepository.authStateChanges(),
+          emits(null),
+        );
+      }),
+    );
+
+    test(
+      "Sign in after dispose throws exceptions",
+      (() {
+        final authRepository = makeFakeAuthRepository();
+        authRepository.dispose();
+        expect(
+          () => authRepository.signInWithEmailAndPassword(
+              testEmail, testPassword),
+          throwsStateError,
+        );
+      }),
+    );
   });
 }
