d6442a182f6d013ed68ca2100e6babff1fa49983
Jashwanth-Gowda-R <jashwanth.go@gmail.com>
2022-12-13 20:27:17 +0530

    Working with WidgetTester and finder

--- test/src/features/authentication/presentation/account/account_screen_test.dart
+++ test/src/features/authentication/presentation/account/account_screen_test.dart
@@ -4,6 +4,7 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
+
   testWidgets('cancel logout', (tester) async {
     await tester.pumpWidget(
       const ProviderScope(
@@ -11,5 +12,16 @@ void main() {
         home: AccountScreen(),
       )),
     );
+    final logoutButton = find.text("Logout");
+    expect(logoutButton, findsOneWidget);
+    await tester.tap(logoutButton);
+    await tester.pump();
+    final dialogTitle = find.text("Are you sure?");
+    expect(dialogTitle, findsOneWidget);
+    final cancelTitle = find.text("Cancel");
+    expect(cancelTitle, findsOneWidget);
+    await tester.tap(cancelTitle);
+    await tester.pump();
+    expect(dialogTitle, findsNothing);
   });
 }
