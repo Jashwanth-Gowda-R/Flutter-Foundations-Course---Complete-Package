1f465e986deab42853e713bc3639650527fef8a2
Jashwanth-Gowda-R <jashwanth.go@gmail.com>
2022-12-14 16:38:07 +0530

    find widgets by key

--- test/src/features/authentication/presentation/account/account_screen_test.dart
+++ test/src/features/authentication/presentation/account/account_screen_test.dart
@@ -28,4 +28,30 @@ void main() {
     // expect(dialogTitle, findsNothing);
     r.expectLogoutDialogNotFound();
   });
+
+  testWidgets('Confirm logout success', (tester) async {
+    // await tester.pumpWidget(
+    //   const ProviderScope(
+    //       child: MaterialApp(
+    //     home: AccountScreen(),
+    //   )),
+    // );
+    var r = AuthRobot(tester);
+    await r.pumpAccountScreen();
+    // final logoutButton = find.text("Logout");
+    // expect(logoutButton, findsOneWidget);
+    // await tester.tap(logoutButton);
+    // await tester.pump();
+    await r.tapLogoutButton();
+    // final dialogTitle = find.text("Are you sure?");
+    // expect(dialogTitle, findsOneWidget);
+    r.expectLogoutDialogFound();
+    // final cancelTitle = find.text("Cancel");
+    // expect(cancelTitle, findsOneWidget);
+    // await tester.tap(cancelTitle);
+    // await tester.pump();
+    await r.tapDialogLogoutButton();
+    // expect(dialogTitle, findsNothing);
+    r.expectLogoutDialogNotFound();
+  });
 }
