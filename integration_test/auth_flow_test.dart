aa29accff0441eed269c8050ff46e25f471858f1
Jashwanth-Gowda-R <jashwanth.go@gmail.com>
2022-12-26 14:41:29 +0530

    Updated integration tests

--- integration_test/auth_flow_test.dart
+++ integration_test/auth_flow_test.dart
@@ -8,15 +8,18 @@ void main() {
   testWidgets('Sign in and sign out flow', (tester) async {
     final r = Robot(tester);
     await r.pumpMyApp();
-    r.expectFindAllProductCards();
+    r.products.expectFindAllProductCards();
+    // r.expectFindAllProductCards();
     await r.openPopupMenu();
     await r.auth.openEmailPasswordSignInScreen();
     await r.auth.signInWithEmailAndPassword();
-    r.expectFindAllProductCards();
+    // r.expectFindAllProductsCards();
+    r.products.expectFindAllProductCards();
     await r.openPopupMenu();
     await r.auth.openAccountScreen();
     await r.auth.tapLogoutButton();
     await r.auth.tapDialogLogoutButton();
-    r.expectFindAllProductCards();
+    r.products.expectFindAllProductCards();
+    // r.expectFindAllProductsCards();
   });
 }
