6495366d78ca12508743cf9a63de6e40f16a51a3
Jashwanth-Gowda-R <jashwanth.go@gmail.com>
2022-12-28 16:12:31 +0530

    Updating the AsyncValueUI extension to handle AppException values

--- lib/src/features/authentication/data/fake_auth_repository.dart
+++ lib/src/features/authentication/data/fake_auth_repository.dart
@@ -1,3 +1,4 @@
+import 'package:ecommerce_app/src/exceptions/app_exception.dart';
 import 'package:ecommerce_app/src/features/authentication/domain/app_user.dart';
 import 'package:ecommerce_app/src/features/authentication/domain/fake_app_user.dart';
 import 'package:ecommerce_app/src/localization/string_hardcoded.dart';
@@ -27,10 +28,12 @@ class FakeAuthRepository {
       }
       // same email, wrong password
       if (u.email == email && u.password != password) {
-        throw Exception('Wrong password'.hardcoded);
+        // throw Exception('Wrong password'.hardcoded);
+        throw const AppException.wrongPassword();
       }
     }
-    throw Exception('User not found'.hardcoded);
+    // throw Exception('User not found'.hardcoded);
+    throw const AppException.userNotFound();
   }
 
   Future<void> createUserWithEmailAndPassword(
@@ -39,12 +42,14 @@ class FakeAuthRepository {
     // check if the email is already in use
     for (final u in _users) {
       if (u.email == email) {
-        throw Exception('Email already in use'.hardcoded);
+        // throw Exception('Email already in use'.hardcoded);
+        throw const AppException.emailAlreadyInUse();
       }
     }
     // minimum password length requirement
     if (password.length < 8) {
-      throw Exception('Password is too weak'.hardcoded);
+      // throw Exception('Password is too weak'.hardcoded);
+      throw const AppException.weakPassword();
     }
     // create new user
     _createNewUser(email, password);
